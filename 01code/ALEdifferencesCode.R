d7f7ee86d637eff33dae261aaaff7f5378244063
Fernando <fernando_colchero@eva.mpg.de>
2024-08-27 11:03:03 +0200

    First commit

---
+++ 01code/ALEdifferencesCode.R
@@ -0,0 +1,180 @@
+# ================================== CODE METADATA =========================== #
+# AUTHORS: Fernando Colchero
+# DATE CREATED: 2024-08-22
+# DESCRIPTION: Reproducibility code for Staerk et al. (2024).
+# NOTES: 
+# ================================ CODE START ================================ #
+# ======================== #
+# ==== GENERAL SETUP: ====
+# ======================== #
+# ----------------------------------------------------- #
+# ---- Libraries, functions and working directory: ----
+# ----------------------------------------------------- #
+# Working directory:
+setwd("Your Path/ALEdifferences/")
+
+# Libraries and functions:
+source("01code/ALEdifferencesFunctions.R")
+
+# -------------------- #
+# ---- Load data: ----
+# -------------------- #
+# Load life tables:
+lifeTabs <- read.csv(file = "02data/tables/LifeTables.csv",
+                     header = TRUE, stringsAsFactors = FALSE)
+
+# Load Siler mortality parameters from maturity:
+thetaMat <- read.csv(file = "02data/tables/SilerParamsFromMatur.csv",
+                     header = TRUE, stringsAsFactors = FALSE)
+
+# Load estimated ALE differences:
+leDiff <- read.csv(file = "02data/tables/ALEdifferences.csv",
+                  header = TRUE, stringsAsFactors = FALSE)
+
+# Load data for BPGLS:
+pglsDat <- read.csv(file = "02data/tables/PLGSdata.csv",
+                  header = TRUE, stringsAsFactors = FALSE)
+
+# Test data for BaSTA analysis:
+survDat <- read.csv(file = "02data/tables/indivTestDat.csv",
+                    header = TRUE, stringsAsFactors = FALSE)
+
+# -------------------------- #
+# ---- Other variables: ----
+# -------------------------- #
+# Sexes:
+sexes <- c("Female", "Male")
+nsex <- length(sexes)
+
+# Plotting colors for sexes:
+sexCol <- c(Female = "#BD0026", Male = "#377EB8")
+
+# ================================ #
+# ==== VISUALIZE LIFE TABLES: ====
+# ================================ #
+# Chose species:
+species <- "Pan troglodytes"
+
+# Find species on life table data:
+idSps <- which(lifeTabs$Species == species)
+
+# Subset life table:
+ltSps <- lifeTabs[idSps, ]
+
+# find X limits:
+xlim <- c(0, max(ltSps$Ages))
+
+# Produce plot of survival, lx:
+par(mfrow = c(1, 1), mar = c(4, 4, 4, 1))
+plot(xlim, c(0, 1), col = NA, xlab = "Age", ylab = "Survival")
+mtext(text = species, side = 3, line = 2, font = 3)
+for (sx in sexes) {
+  idsx <- which(ltSps$Sex == sx)
+  lines(ltSps$Ages[idsx], ltSps$lx[idsx], col = sexCol[sx], type = 's',
+        lwd = 4)
+}
+legend("topright", legend = sexes, lwd = 4, col = sexCol, bty = 'n')
+
+
+# ========================================= #
+# ==== AGE-SPECIFIC SURVIVAL ANALYSIS: ====
+# ========================================= #
+# Find life history data for chosen species:
+idSps <- which(pglsDat$Species == species)
+
+# Find age at maturity:
+alpha <- floor(max(c(pglsDat$FemaleAFB[idSps], pglsDat$MaleAFB[idSps])))
+
+# Run BaSTA on survival data:
+out <- basta(object = survDat, dataType = "census", model = "GO",
+             shape = "bathtub", formulaMort = ~ Sex - 1, minAge = alpha,
+             nsim = 4, parallel = TRUE, ncpus = 4)
+
+# Print output to the console:
+summary(out)
+
+# Plot goodness of fit plots:
+plot(out, plot.type = 'gof')
+
+# ================================================================ #
+# ==== CALCULATE POSTERIOR LIFE EXPECTANCY FROM BaSTA OUTPUT: ==== 
+# ================================================================ #
+# Calculate posterior for ALE per sex and of ALE differences:
+exFull <- CalcPostALE(out, ncpus = 4)
+
+# Calculate summary statistics (posterior mean, SD, and quantiles):
+quants <- CalcQuants(object = exFull)
+
+# Print results to the console:
+print(quants)
+
+# ================================================ #
+# ==== EVOLUTIONARY PREDICTORS OF ALE DIFFS.: ====
+# ================================================ #
+# Subset data to only include Artiodactyls:
+idOrder <- which(pglsDat$Order == "Artiodactyla")
+pglsDatOrder <- pglsDat[idOrder, ]
+
+# Run BPGLS:
+outpgls <- RunBayesPGLS(formula = exDiff ~ log(MaleBM) + log(FemaleBM) + MS, 
+                        data = pglsDatOrder, weights = "weights",
+                        phylo = fullphylo, ncpus = 6, nsim = 6)
+
+# Print summary:
+outpgls
+
+# Plot parameter posterior densities:
+plot(outpgls, plot.type = "density")
+
+# ============================================ #
+# ==== EFFECT OF ALLOMETRY ON REGRESSION: ==== 
+# ============================================ #
+# ---------------------------- #
+# Allometric relation (e.g., testes vs male body mass): Y = alpha * X^gamma
+# Predictor (e.g., RL): XY = log(Y) - gamma * log(X)
+# ---------------------------- #
+# Number of observations:
+n <- 1000
+
+# Alpha:
+al <- 1
+
+# Gamma:
+gam <- 0.8
+
+# Simulate X (e.g., male body mass)
+x <- runif(n = n, 1, 100)
+
+# Variability in allometry:
+del <- 2
+
+# Simulate Y (e.g., testes mass)
+y <- exp(log(al) + gam * log(x) + rnorm(n = n, mean = 0, sd = del))
+
+# Simulate predictor (e.g., relative testes mass, RL):
+xy <- log(y) - gam * log(x)
+
+# Regression coefficients between response and predictor (e.g., ALE diffs ~ RL)
+sig <- 0.5
+eta <- 0.5
+bet <- 1
+
+# Simulate response Z (e.g., ALE diffs.):
+z <- eta + bet * xy + rnorm(n = 100, mean = 0, sd = sig)
+
+# Run regression between Z and log Y, log X:
+reg1 <- lm(z ~ log(y) + log(x))
+
+# Run regression between Z and XY (e.g., RL):
+reg2 <- lm(z ~ xy)
+
+# Verify that beta in regression 3 is equal to beta1 in regression 2 and equal 
+# to beta used for simulation:
+bet
+reg2$coefficients[2]
+reg1$coefficients[2]
+
+# Verify that gamma can be retrieved from the ratio -beta_2 / beta_1 
+# in regression 2:
+gam
+-reg1$coefficients[3] / reg1$coefficients[2]
---
+++ 01code/ALEdifferencesFunctions.R
@@ -0,0 +1,169 @@
+# ================================== CODE METADATA =============================
+# AUTHOR: Fernando Colchero
+# DATE CREATED: 2024-08-26
+# DATE MODIFIED: 
+# DESCRIPTION: Functions for reproducibility code for Staerk et al. (2024)
+# NOTES: Uses packages:
+#           - paramDemo (parametric and non-parametric demographic functions)
+#           - BaSTA (Bayesian survival trajectory analysis)
+#           - snowfall (parallel computing)
+#           - BayesPGLS (Bayesian Phylogenetic Generalized Least Squares)
+#           - phytools ()
+#           - caper ()
+# ================================ CODE START ==================================
+# ==================== #
+# ==== LIBRARIES: ====
+# ==================== #
+# --------------------------- #
+# ---- Install packages: ----
+# --------------------------- #
+# Vector of installed packages:
+instPacks <- installed.packages()[, 1]
+
+# snowfall for parallel computing:
+if (!"snowfall" %in% instPacks) {
+  install.packages("snowfall")
+}
+library(snowfall)
+
+# Install and load 'BaSTA':
+if (!"BaSTA" %in% instPacks) {
+  install.packages("BaSTA")
+}
+library(BaSTA)
+
+# Install and load 'paramDemo':
+if (!"paramDemo" %in% instPacks) {
+  install.packages("paramDemo")
+}
+library(paramDemo)
+
+# Install and load 'devtools':
+if (!"devtools" %in% instPacks) {
+  install.packages("devtools")
+}
+library(devtools)
+
+# Install and load 'BayesPGLS':
+if (!"BayesPGLS" %in% instPacks) {
+  install_git("https://github.com/fercol/BayesPGLS", subdir = "pkg/")
+}
+library(BayesPGLS)
+
+# Install and load 'phytools':
+if (!"phytools" %in% instPacks) {
+  install.packages("phytools")
+}
+library(phytools)
+
+# Install and load 'caper':
+if (!"caper" %in% instPacks) {
+  install.packages("caper")
+}
+library(caper)
+
+
+# =============================== #
+# ==== ADDITIONAL FUNCTIONS: ==== 
+# =============================== #
+# Demographic rates:
+surv <- function(th, x) {
+  exp(exp(th["a0"]) / th["a1"] * (exp(-th["a1"] * x) - 1) - th["c"] * x +
+        exp(th["b0"]) / th["b1"] * (1 - exp(th["b1"] * x)))
+}
+
+mort <- function(th, x) {
+  exp(th["a0"] - th["a1"] * x) + th["c"] + exp(th["b0"] + th["b1"] * x)
+}
+
+# Parallel function for life expectancy:
+exParal <- function(isim, theFem, theMal, surv, mort, xv, dx, ncpus) {
+  nthe <- nrow(theFem)
+  idsim <- seq(0, nthe, nthe / ncpus)
+  isim <- 1
+  
+  id <- (idsim[isim] + 1):idsim[isim + 1]
+  # Remaining life expectancy:
+  exFull <- t(sapply(id, function(ith) {
+    thef <- theFem[ith, ]
+    them <- theMal[ith, ]
+    Sxf <- surv(th = thef, x = xv) / 
+      surv(th = thef, x = xv[1])
+    Sxm <- surv(th = them, x = xv) / 
+      surv(th = them, x = xv[1])
+    exf <- sum(Sxf * dx)
+    exm <- sum(Sxm * dx)
+    maxex <- max(c(exf, exm))
+    exdiff <- (exf - exm) / maxex
+    return(c(exFemale = exf, exMale = exm, exDiff = exdiff))
+  }))
+  return(exFull)
+}
+
+# Calculate posterior life expectancy and sex differences:
+CalcPostALE <- function(out, ncpus = 4) {
+  # Age vector for integration:
+  dx <- 0.005
+  x <- seq(0, 150, dx)
+  
+  # Extract Siler mortality parameters (theta) per sex:
+  theFem <- out$params[, grep("Female", colnames(out$params))]
+  theMal <- out$params[, grep("Male", colnames(out$params))]
+  nthe <- nrow(theFem)
+  colnames(theFem) <- colnames(theMal) <- c("a0", "a1", "c", "b0", "b1")
+  
+  # Remaining life expectancy from each converged MCMC iteration:
+  sfInit(parallel = TRUE, cpus = ncpus)
+  outparal <- sfClusterApplyLB(x = 1:ncpus, exParal, theFem = theFem, 
+                               theMal = theMal, mort = mort, surv = surv, 
+                               xv = x, dx = dx, ncpus = ncpus)
+  sfStop()
+  
+  # Extract parallel runs into a single data frame:
+  for (isim in 1:ncpus) {
+    if (isim == 1) {
+      exFull <- outparal[[isim]]
+    } else {
+      exFull <- rbind(exFull, outparal[[isim]])
+    }
+  }
+  return(exFull)
+}
+
+# Calculate quantiles of ALE differences:
+CalcQuants <- function(object) {
+  # Summary statistics names:
+  statsnames <- c("Mean", "SD", "Lower", "Upper", "zeroOverlap")
+  nstats <- length(statsnames)
+  
+  # Variables:
+  varNames <- c("ALE_Female", "ALE_Male", "ALEdiff") 
+  nvar <- length(varNames)
+  
+  # Calculate summary statistics (posterior mean, SD, and quantiles):
+  quants <- matrix(NA, nrow = nvar, ncol = nstats,
+                   dimnames = list(varNames, statsnames))
+  for (ii in 1:ncol(object)) {
+    xi <- object[, ii]
+    qi <- c(Mean = mean(xi, na.rm = TRUE), 
+            SD = sd(xi, na.rm = TRUE), 
+            Lower = quantile(xi, 0.025, na.rm = TRUE),
+            Upper = quantile(xi, 0.975, na.rm = TRUE), NA)
+    names(qi) <- sprintf("%s_%s", colnames(object)[ii], statsnames)
+    quants[ii, ] <- qi
+  }
+  
+  # Calculate zero overlap for ALE differences:
+  pv <- pnorm(q = 0, mean = quants["ALEdiff", "Mean"],
+              sd = quants["ALEdiff", "SD"])
+  if (0 < quants["ALEdiff", "Mean"]) {
+    peq <- 2 * pv
+  } else {
+    peq <- 2 * (1 - pv)
+  }
+  
+  # Store results:
+  quants[3, nstats] <- peq
+  
+  return(quants)
+}
